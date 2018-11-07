f8107ce018f6e6efe1c075bb96f173402afeee01
yuya.fyy <yuya.fyy@alibaba-inc.com>
2018-11-07 11:54:10 +0800

    Merge branch 'master' of https://github.com/fengyy51/bisheadminmaster
    # Conflicts:
    #	src/main/java/com/binwang/dao/AnalysisDao.java

---
+++ src/main/java/com/binwang/dao/AnalysisDao.java
@@ -0,0 +1,19 @@
+package com.binwang.dao;
+
+import com.binwang.bean.analysis.UserCollect;
+import com.binwang.bean.analysis.Userprize;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface AnalysisDao {
+
+    @Select("SELECT count(id) as num,add_time as addtime from f_user_prize GROUP BY Time(FROM_UNIXTIME(add_time))")
+    public List<Userprize>listUserPrize();
+    @Select("SELECT count(id) as num,add_time as addtime from f_collect GROUP BY Time(FROM_UNIXTIME(add_time))")
+    public List<UserCollect>listUserCollect();
+}
