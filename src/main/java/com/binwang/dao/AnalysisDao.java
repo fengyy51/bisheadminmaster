439fb6a43d0bcc5cb5ca98759029663e6a865d64
zijinggao <654905065@qq.com>
2018-11-07 11:47:45 +0800

    增加信息采集功能

--- src/main/java/com/binwang/dao/AnalysisDao.java
+++ src/main/java/com/binwang/dao/AnalysisDao.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+//yy
 @Repository
 @Mapper
 public interface AnalysisDao {
